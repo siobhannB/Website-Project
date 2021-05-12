fd2b84350491f9de73244838b15ba013e9c77a3d
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-05-12 12:23:57 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="container-fluid"> <!--div for the main image-->
+      <div class="container">
+          <div class="row">
+          <div class="col-md-6">
+              <p id="contactinfo">Office: 36 WaterRoad Fremantle WA 2100 <br>
+               Postal: PO Box 236 Nowhere WA 2100 <br>
+               Telephone: 61 2 6939 6100 <br>
+               Email: Bradn@fremantleartscentre.com.au
+               </p> 
+              </div>
+              <div class="col-md-6 text-center">
+           <!--   social media icons to be placed in this section-->
+                  <img id="facebook" src="http://206.189.45.97/~mesh1/wp-content/themes/fremantle_theme_wp_fiinished/images/facebook.jpg" alt="facebook"> <!--image needs to have clear background-->
+                  
+                  <img id="instagram" src="http://206.189.45.97/~mesh1/wp-content/themes/fremantle_theme_wp_fiinished/images/instagram.jpg" alt="instagram">
+                  
+                  <img id="twitter" src="http://206.189.45.97/~mesh1/wp-content/themes/fremantle_theme_wp_fiinished/images/twitter.jpg" alt="twitter">
+              </div>
+          
+          </div> 
+         <!-- row-->
+          </div>
+         <!-- container-->
+      
+      </footer>
+
+
+  </body>
+</html>
+
+<?php wp_footer(); /* Tells WordPress to include footer.php */ ?>
