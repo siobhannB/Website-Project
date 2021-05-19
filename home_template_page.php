63747b2e0e1aa62f8ad4542d99f8e738aebfc5dd
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-05-19 12:52:05 +0800

    Create home_template_page.php

---
+++ home_template_page.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Template Name: home-page
+*/
+?>
+   <?php get_header(); /* Tells WordPress to include header.php */ ?>
+      
+    
+      <section class="container-fluid eventsbg text-center ">
+      <div class="container eventscontent">
+          <div class="row">
+            <div>
+          <h2>Events On Now</h2>
+            </div>
+              
+          <div class="col-md-4">
+               <h3>Spacey Jane: MUSIC</h3>
+          <p id="spaceyjane">Following huge demand for their first show, Spacey Jane announced a second date on their forthcoming national Sunlight tour. They're joined by special guests Jack Davies and the Bush Chooks.</p>
+          <p id="date1">Sat 10 Apr  2021  |  6:00pm — 10:00pm | South Lawn</p>
+              <img src="images/event1.png" alt="spaceyjane">
+                <button type="button" class="btn2">Find Out More</button>
+           </div>
+              
+              <div class="col-md-4">
+          <h4>RESCHEDULED Ocean Alley: MUSIC</h4>
+          <p id="oceanalley">Ocean Alley are stoked to reveal their first national tour of Australia since 2018 winds up on FAC’s South Lawn on Sat 11 Sep, <br> supported by Psychedelic Porn Crumpets and Dulcie.</p>
+          <p id="date2">Sat 11 Sep 2021  |  6:00pm — 10:00pm |South Lawn</p>
+                  <img src="images/event2.png" alt="oceanalley">
+                    <button type="button" class="btn3">Find Out More</button>
+          </div>
+              
+            <div class="col-md-4">
+          <h5>New And Emerging Aboriginal Artists: EXHIBITION</h5>
+          <p id="aboriginalartists">Presenting the best new and emerging Aboriginal artists from remote, regional and metro WA, Revealed 2021 will showcase artworks across painting, installation, textiles and photography</p>
+          <p id="date3">Fri  26 Mar 2021-Sun 23 May 2021  |  10:00am — 5:00pm | South Wing and Collection Galleries</p>
+                <img src="images/event3.png" alt="new and emerging aboriginal artists">
+                  <button type="button" class="btn4">Find Out More</button>
+        </div>
+              
+              
+              
+          </div>
+         <!-- row-->
+          </div>
+       <!--   container-->
+    
+      </section>
+      
+      <section class="container-fluid creativitybg">
+      <div class="container">
+          <div class="row">
+         <div class="col-md-6">
+           <h6>Unlock Your <br>Creativity</h6>
+          <p id="artcentre">Fremantle Art Centre has a range of <br> creative art courses suited for both <br> adults and kids. Whether you like <br> making jewellery, art prints or <br> sculptures, there is a course for <br> everyone and anyone.</p>
+             <button type="button" class="btn5">Find Out More</button>
+            </div> <!--col div-->
+          </div> <!--row div-->
+    
+          </div>
+         <!-- container div-->
+      </section>
+      
+       <section class="container-fluid historybg">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-6 col-sm-1"></div>
+              <div class="col-md-6 col-sm-11 text-right historyblock">
+          <h7>Our History</h7>
+          <p id="historytext">Housed in a gothic building built in the 1860s, the <br> site now known as the Fremantle Arts Centre has a <br> long and fascinating history. Built by convicts on <br> a grant of six acres, this splendid example of <br>Australian Gothic architecture first opened its <br> doors in 1864, as the Convict Establishment <br> Fremantle Lunatic Asylum and Invalid Depot. <br></p>
+              
+          <p id="historytext2">In 1958, the old Fremantle Lunatic Asylum was <br> finally threatened with demolition and was saved <br> largely through the efforts of Sir Frederick <br> Samson, then Mayor of Fremantle. His vision for <br> the site was to establish both a Mariners’ Museum <br> and an Arts Centre. The building was restored <br> and the museum stage was finally completed in <br> 1970. <br> </p>
+              
+          <p id="historytext3">Fremantle Arts Centre opened in 1973, with Ian <br> Templeman as Founding Director. Templeman <br> relied on minimal resources and skeleton staff for <br> support and, over his 17 years at the helm, <br> developed Fremantle Arts Centre’s first programs <br> and services, including Fremantle Arts Centre<br>   Press.</p>
+                  <button type="button" class="btn6">Read More</button>
+          </div> <!--col div-->
+             </div> <!--row div -->
+          </div>
+          <!-- container div-->
+      </section>
+         
+<?php get_footer(); ?>
