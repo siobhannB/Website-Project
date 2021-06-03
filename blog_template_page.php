a02fc68abb5aaa7e1be83038bd0dcb2b42848feb
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-06-03 10:34:10 +0800

    Update blog_template_page.php

--- blog_template_page.php
+++ blog_template_page.php
@@ -7,33 +7,33 @@ Template Name: Blog
 <div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
-            <section class="col-sm-7 maintext">				
-    <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-<div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
-
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
-		
-	</div><!--post class-->
-
-   
-<?php endwhile; ?>
-	
-	<?php else: ?>
-	<div class="post">
-		<p>Sorry, no posts found.</p>
-	</div><!--post-->
-	<?php endif; ?>
-	</article><!---->
-</section>
+            <?php $post_id = 5578;
+$queried_post = get_post($post_id);?>
+<div class="col-md-6"><?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?></div>
+<div class="col-md-6">
+			<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<a href ="http://206.189.45.97/~mesh5/5578/" class="btn7" type="button">Read More</a>  
+			</div>
            
+            <?php $post_id = 5585;
+$queried_post = get_post($post_id);?>
+<div class="col-md-6"><?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?></div>
+<div class="col-md-6">
+			<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<a href ="http://206.189.45.97/~mesh5/5585/" class="btn8" type="button">Read More</a>              
+			</div>
+            
+                <?php $post_id = 5587;
+$queried_post = get_post($post_id);?>
+<div class="col-md-6"><?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?></div>
+<div class="col-md-6">
+			<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<a href ="http://206.189.45.97/~mesh5/5587/" class="btn9" type="button">Read More</a>                     
+			</div>
+            
      </div>
     </div>
   </div>
