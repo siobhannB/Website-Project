1bc2c543da83a9d07a0c547948704e97ef1307fc
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-06-04 15:03:45 +0800

    Update services_template_page.php

--- services_template_page.php
+++ services_template_page.php
@@ -4,6 +4,62 @@ Template Name: Services
 */
 ?>
 <?php include (TEMPLATEPATH . '/small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?> 
+
+<div class="container-fluid small_herobg">
+  <div class="container text-center">
+        
+        <h1>Jam Out to <br> Jazz, Classical, Reggae and Rock</h1>
+    
+        <div class="row">
+    
+      <div class="col-md-6">   <img id="music" src="images/music%20applications.png" alt="musicapplications"> </div>
+        <div class="col-md-6">
+               <h9>Sunday Music <br>Applications</h9>
+          <p id="sundaymusic">Sunday Music is a free concert series <br>which takes place at Fremantle Arts Centre from 2–4pm every Sunday from October–March and aims to showcase a broad range of musical styles and genres as well as providing a platform for both emerging and established artists.</p>
+          
+           <button type="button" class="btn10">Further Information</button>
+           </div>
+      </div>  
+      
+      
+      <div class="row">
+        <div class="col-md-6">
+               <h9>Exhibition Proposals</h9>
+          <p id="proposals">Fremantle Arts Centre welcomes exhibition proposals from individual artists, groups of artists, curators and organisations. FAC will consider gallery-based, off-site and online projects.</p>
+             
+           <button type="button" class="btn11">Further Information</button>
+           </div>
+          <div class="col-md-6">
+          <img id="exhbitions" src="images/exhibitionproposals.png" alt="exhibitions"> 
+          </div>
+          
+      </div>
+      
+      
+         <div class="row">
+             
+             <div class="col-md-6"> <img id="hire" src="images/venuehire.png" alt="venuehire"> </div>
+             
+        <div class="col-md-6">
+               <h9>Venue Hire</h9>
+          <p id="venuehire">FAC offers three unique spaces for concert hire, year round – the South Lawn, Front Garden and Inner Courtyard. We can cater for high-profile international acts, national and local performers. Previous acts include Nick Cave, Billie Eilish, RÜFÜS DU SOL, Lauryn Hill, Chet Faker, Passenger, James Bay, Hot Chip, Paul Kelly, The Waifs, Archie Roach, PJ Harvey and Flying Lotus.</p>
+            
+           <button type="button" class="btn12">Further Information</button>
+           </div>
+      
+    
+      </div>
+
+  </div>
+
+  </div>
+
+
+
+
+
+
+
 <div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
@@ -38,5 +94,6 @@ $queried_post = get_post($post_id);?>
            
      </div>
     </div>
-  </div>
+
+
 <?php get_footer(); ?>
